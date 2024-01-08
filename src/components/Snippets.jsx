d87c292ef4980323cba1597071563aae14a33c5a
Bharath Gopalswamy <110247438+bharathgopalswamy@users.noreply.github.com>
2024-01-08 12:29:13 +0530

    update

--- src/components/Snippets.jsx
+++ src/components/Snippets.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import 'react-quill/dist/quill.snow.css'; // Import the styles
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; 
 import './css/Snippets.css';
 
 const Snippets = () => {
@@ -13,33 +14,22 @@ const Snippets = () => {
     setEditorContent('');
   };
 
-  const handleCommand = (command, value = null) => {
-    document.execCommand(command, false, value);
+  const modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'list', 'link', 'undo', 'redo'],
+      [{ 'header': [1, 2, 3] }],
+    ],
   };
 
   return (
     <div>
-      <div className="navbar">
-        <button onClick={() => handleCommand('bold')}>B</button>
-        <button onClick={() => handleCommand('italic')}>I</button>
-        <button onClick={() => handleCommand('underline')}>U</button>
-        <button onClick={() => handleCommand('formatBlock', 'h1')}>H1</button>
-        <button onClick={() => handleCommand('formatBlock', 'h2')}>H2</button>
-        <button onClick={() => handleCommand('formatBlock', 'h3')}>H3</button>
-        <button onClick={() => handleCommand('insertUnorderedList')}>List</button>
-        <button onClick={() => handleCommand('createLink', prompt('Enter the URL:'))}>Link</button>
-        <button onClick={() => handleCommand('undo')}>Undo</button>
-        <button onClick={() => handleCommand('redo')}>Redo</button>
-        <div className="nav-item">Comments</div>
-      </div>
+      <ReactQuill
+        value={editorContent}
+        onChange={(value) => setEditorContent(value)}
+        modules={modules}
+        placeholder="Type your comment"
+      />
       <form onSubmit={addComment}>
-        <div
-          contentEditable
-          name="content"
-          className="editor"
-          placeholder="Type your comment"
-          onInput={(e) => setEditorContent(e.target.innerHTML)}
-        ></div>
         <button className='btn btn-primary commentbtn' type="submit">Add Comment</button>
       </form>
       <ul>
